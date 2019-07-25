b8ab050fc875c7f9875ffa042fdcf9129af0e5c0
Jordan Roberts <jordanroberts@talk21.com>
2019-07-25 16:46:14 +0100

    Contains method returns a boolean t/f if string was found y/n

--- Strings/Strings/Strings.cs
+++ Strings/Strings/Strings.cs
@@ -6,19 +6,14 @@ namespace Strings
     {
         static void Main()
         {
-            string sayHello = "Hello World!";
-            Console.WriteLine(sayHello);
-            sayHello = sayHello.Replace("Hello", "Konnichiwa");
-            Console.WriteLine(sayHello);
-            Console.WriteLine(sayHello.ToUpper());
-            Console.WriteLine(sayHello.ToLower());
+            string songLyrics = "We will we will rock you";
+            Console.WriteLine(songLyrics.Contains("rock"));
+            Console.WriteLine(songLyrics.Contains("eat"));
         }
     }
 }
 
 // Output:
 
-//Hello World!
-//Konnichiwa World!
-//KONNICHIWA WORLD!
-//konnichiwa world!
+//True
+//False
