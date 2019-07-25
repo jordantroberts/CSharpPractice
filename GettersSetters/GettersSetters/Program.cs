6f7fda8f92f59fd71fac835f3a9de53bf173e5dd
Jordan Roberts <jordanroberts@talk21.com>
2019-07-25 18:38:06 +0100

    Added GettersSetters file to explore getters and setters in C#.
    At the moment a simple program to create some movies. Problem is that ratings will only be set values such as PG, 12, 15, 18. At the moment, someone could come in and set the rating to "dog". So we need to change this.

---
+++ GettersSetters/GettersSetters/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace GettersSetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "12");
+            Movie shrek = new Movie("Shrek", "Adam adamson", "PG");
+
+            Console.WriteLine(avengers.title);
+            Console.ReadLine();
+      
+
+        }
+    }
+}
