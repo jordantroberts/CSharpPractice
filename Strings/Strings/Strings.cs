f1c8f7700f7a881464ddfd654ee5a88be236ff84
Jordan Roberts <jordanroberts@talk21.com>
2019-07-25 14:46:49 +0100

    Experimenting with string methods

--- Strings/Strings/Strings.cs
+++ Strings/Strings/Strings.cs
@@ -11,6 +11,7 @@ namespace Strings
             Console.WriteLine($"My friends are {aFriend} and {newFriend}");
             Console.WriteLine($"The name {aFriend} has {aFriend.Length} letters.");
             Console.WriteLine($"The name {newFriend} has {newFriend.Length} letters.");
+            Console.WriteLine($"Hey, {aFriend.Replace("Amy", "Rhiannon")}");
         }
     }
 }
