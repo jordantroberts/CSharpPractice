4fc2348094d5010ea81baae558191ecacd1a8d99
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 16:26:39 +0100

    Quotients and modulus

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -6,12 +6,20 @@ namespace Numbers
     {
         static void Main()
         {
-            int a = 18;
-            int b = 6;
-            int c = a + b;
-            Console.WriteLine(c);
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c; 
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"quotient: {e}");
 
-         
+            
         }
     }
 }
+
+// Output:
+
+    // 3
+    // 2 
