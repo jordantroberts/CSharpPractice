ec91c7e835e58ac9d4315dcc8a7ec4e0a2c41c24
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 11:54:55 +0100

    Added a songCount STATIC ATTRIBUTE to the Song class. A static attribute is an attribute about the actual class itself.

--- StaticClassAttributes/StaticClassAttributes/Program.cs
+++ StaticClassAttributes/StaticClassAttributes/Program.cs
@@ -6,7 +6,14 @@ namespace StaticClassAttributes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+
+            Console.WriteLine(Song.songCount);
+            // can call songCount as it's an attribute of the Song class. You cannot call "Song.title" for example
+
+            Console.ReadLine(); 
+
         }
     }
 }
--- StaticClassAttributes/StaticClassAttributes/Song.cs
+++ StaticClassAttributes/StaticClassAttributes/Song.cs
@@ -8,8 +8,10 @@ namespace StaticClassAttributes
         public string title;
         public string artist;
         public int duration;
-
-        public Song(string aTitle, string aAttist, int aDuration)
+        // Create a static attribute to tell us how many song objects have been created:
+        public static int songCount = 0;
+       
+        public Song(string aTitle, string aArtist, int aDuration)
         {
             title = aTitle;
             artist = aArtist;
@@ -17,3 +19,6 @@ namespace StaticClassAttributes
         }
     }
 }
+
+// Another type of attribute is a static attribute, which isn't unique to each of the objects.
+// In other words, a static attribute is an attribute about the actual class. Will be the same across all of the objects of the class.
