bf04cac99734915cee3136101159c5b09a5ae3f6
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 11:57:14 +0100

    Added to the Song constructor method - any time a Song object gets created, this method is called. So now every time a new Song object is created and the constructor method gets called, the songCount static attribute incremements as it's counting how many Song objects are being created.

--- StaticClassAttributes/StaticClassAttributes/Song.cs
+++ StaticClassAttributes/StaticClassAttributes/Song.cs
@@ -8,17 +8,21 @@ namespace StaticClassAttributes
         public string title;
         public string artist;
         public int duration;
+        // Another type of attribute is a static attribute, which isn't unique to each of the objects.
+        // In other words, a static attribute is an attribute about the actual class. Will be the same across all of the objects of the class.
         // Create a static attribute to tell us how many song objects have been created:
         public static int songCount = 0;
-       
+
+        // This is a constructor method - any time a Song object gets created, this method is called. 
         public Song(string aTitle, string aArtist, int aDuration)
         {
             title = aTitle;
             artist = aArtist;
             duration = aDuration;
+            // every time we create a song, let's increment the song count:
+            songCount ++ 
         }
     }
 }
 
-// Another type of attribute is a static attribute, which isn't unique to each of the objects.
-// In other words, a static attribute is an attribute about the actual class. Will be the same across all of the objects of the class.
+
