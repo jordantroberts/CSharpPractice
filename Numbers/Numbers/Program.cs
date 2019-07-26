9337e4c7feacc46e8386cb78f5a0686cb1538f28
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 16:21:13 +0100

    Set up file to explore numbers in C#

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+
+         
+        }
+    }
+}
