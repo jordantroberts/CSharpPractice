4ab39777409a9552a8981a559da344f0cbf82838
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 13:39:43 +0100

    Explaining that you can't create an instance of a static class

--- StaticMethodsAndClasses/StaticMethodsAndClasses/Program.cs
+++ StaticMethodsAndClasses/StaticMethodsAndClasses/Program.cs
@@ -6,6 +6,8 @@ namespace StaticMethodsAndClasses
     {
         static void Main(string[] args)
         {
+            Math = myMath = new Math();
+            // YOU CANNOT CREATE AN INSTANCE OF THE MATH CLASS AS THE MATH CLASS IS A STATIC CLASS
             UsefulTools.SayHi("Jordan");
 
             // Didn't actually have to create an instance of the UsefulTools class!
