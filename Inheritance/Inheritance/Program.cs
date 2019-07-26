37ddf1944d96171173705b3b1ea47d6dc729cf40
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 13:45:20 +0100

    Added chef class with 3 methods it can use

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -13,3 +13,6 @@ namespace Inheritance
         }
     }
 }
+
+// Inheritance is basically a technique we can use in C# where we can have one class inherit all of the functionality from another class.
+// We can define a SUPERCLASS and create classes that can inherit all the functionality from that superclass, called subclasses.
