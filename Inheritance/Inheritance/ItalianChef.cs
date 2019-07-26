a5688780cedc206aec9c3a0bc03b4e7e71ba7b8a
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 13:49:38 +0100

    Added ': Chef' to ItalianChef class so that the Italian Chef inherits all the attributes and functionality of a normal chef. Use inheritance rather than copy and pasting

--- Inheritance/Inheritance/ItalianChef.cs
+++ Inheritance/Inheritance/ItalianChef.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 namespace Inheritance
 {
-    public class ItalianChef
+     class ItalianChef : Chef
     {
-        public ItalianChef()
         {
+        // This Italian chef can do everything a normal chef can do. It can also MakeChicken, it can also MakeSalad and SpecialDish.
+        // It's very similar to a normal chef but he can also do extra (Italian) stuff.
+        // You could copy and paste the methods from the Chef class BUT in C# we don't have to and can use inheritance - the process of inheriting the functionality and attributes from another class.
+        // Do this by adding : Chef
+
         }
     }
 }
