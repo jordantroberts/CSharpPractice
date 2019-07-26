d51aa8e4fb4415c972065d834c3d66a8637af90b
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 16:36:18 +0100

    Calculating area of a circle

--- Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Program.cs
@@ -6,22 +6,14 @@ namespace Numbers
     {
         static void Main()
         {
-            double a = 1.0;
-            double b = 3.0;
-            Console.WriteLine(a / b);
+            // area of a circle is radius squared multipled by PI. 
 
-            // The M suffix is how you indicate that a constant should use the decimal type. 
-            decimal c = 1.0M;
-            decimal d = 3.0M;
-            Console.WriteLine(c / d);
+            double radius = 2.50;
+
+            double area = (radius * radius) * Math.PI;
+
+            Console.WriteLine(area);
+
         }
     }
 }
-
-// Output:
-
-// 0.333333333333333
-// 0.3333333333333333333333333333
-
-// Using a decimal type has more digits to the right of the decimal point.
-// The decimal type has a smaller range but greater precision than a double.
