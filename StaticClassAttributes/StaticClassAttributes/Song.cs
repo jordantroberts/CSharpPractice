304a1219b6789a80e02b0d9dace76fa4b52f9958
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 11:49:59 +0100

    Learning about static attributes. Set up of basic Song class with normal attributes.

---
+++ StaticClassAttributes/StaticClassAttributes/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace StaticClassAttributes
+{
+     class Song
+    {
+        // Normal attributes on Song class. If you type 'Console.WriteLine(holiday.title)' and Console.WriteLine(kashmir.title) it will print the different titles for each Song.
+        public string title;
+        public string artist;
+        public int duration;
+
+        public Song(string aTitle, string aAttist, int aDuration)
+        {
+            title = aTitle;
+            artist = aArtist;
+            duration = aDuration;
+        }
+    }
+}
