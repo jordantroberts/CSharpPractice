bdd11b8d1d449047752223fc2a884ddd3134f694
Jordan Roberts <jordanroberts@talk21.com>
2019-07-26 11:51:32 +0100

    Each song will have its own title, its own artist and own duration. These things will differ across objects.

--- StaticClassAttributes/StaticClassAttributes/Song.cs
+++ StaticClassAttributes/StaticClassAttributes/Song.cs
@@ -4,6 +4,7 @@ namespace StaticClassAttributes
      class Song
     {
         // Normal attributes on Song class. If you type 'Console.WriteLine(holiday.title)' and Console.WriteLine(kashmir.title) it will print the different titles for each Song.
+        // Each song will have its own title, its own artist and own duration. These things will differ across objects. 
         public string title;
         public string artist;
         public int duration;
