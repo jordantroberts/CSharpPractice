1b39a79f556af9662822afa083c4cb807140c3eb
Jordan Roberts <jordanroberts@talk21.com>
2019-07-27 17:22:29 +0100

    Simple while loop with counter

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine($"Hello world! The counter is {counter}");
+                counter++;
+            }
+        }
+    }
+}
+
+//Output = 
+//Hello world! The counter is 0
+//Hello world! The counter is 1
+//Hello world! The counter is 2
+//Hello world! The counter is 3
+//Hello world! The counter is 4
+//Hello world! The counter is 5
+//Hello world! The counter is 6
+//Hello world! The counter is 7
+//Hello world! The counter is 8
+//Hello world! The counter is 9
