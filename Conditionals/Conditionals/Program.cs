9cdaf55315b2e40ccf8bfb2eaedcacfcc29cb75e
Jordan Roberts <jordanroberts@talk21.com>
2019-07-27 17:10:47 +0100

    Set up file for learning about conditionals in C#

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+            if (a + b > 10)
+                Console.WriteLine("The answer is greater than 10");
+            else
+                Console.WriteLine("The answer is not greater than 10");
+        }
+    }
+}
