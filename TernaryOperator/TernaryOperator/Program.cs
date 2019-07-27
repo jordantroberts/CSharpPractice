9e256422ba4722a3b895f5ce3c6afbd04e467c91
Jordan Roberts <jordanroberts@talk21.com>
2019-07-27 20:16:09 +0100

    Ternary operators in C#

---
+++ TernaryOperator/TernaryOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace TernaryOperator
+{
+	class Program
+	{
+		static void Main(string[] args)
+
+        //You’re growing peppers and wrote a program that lets you know if it’s time to pick them.
+        //If a pepper is at least 3.5 inches, it’s time to be picked.
+        //If it’s not ready, the program should tell you to “wait a little longer”.
+
+        {
+            int pepperLength = 4;
+
+			string message = (pepperLength >= 3.5) ? "ready!" : "wait a little longer";
+
+			Console.WriteLine(message);
+
+		}
+	}
+}
