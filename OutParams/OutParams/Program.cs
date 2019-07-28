fe9acc7490364397e3fcbb7806acbc3085237e51
Jordan Roberts <jordanroberts@talk21.com>
2019-07-28 16:53:09 +0100

    Using out parameter for when you want to return multiple values

---
+++ OutParams/OutParams/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OutParameters
+{
+	class Program
+	{
+        static void Main(string[] args)
+        {
+            string ageAsString = "102";
+            string nameAsString = "Granny";
+            int ageAsInt;
+            bool outcome;
+
+            // For example the Int32.TryParse() method tries to parse its input as an integer.
+            // If it can, it returns true and sets a specific variable to the new value.
+            outcome = Int32.TryParse(ageAsString, out ageAsInt);
+
+            Console.WriteLine(outcome);
+            Console.WriteLine(ageAsInt);
+
+            int nameAsInt;
+            bool outcome2;
+
+            outcome2 = Int32.TryParse(nameAsString, out nameAsInt);
+
+            Console.WriteLine(outcome2);
+            Console.WriteLine(nameAsInt);
+
+
+        }
+    }
+}
+
+// Output:
+
+//True
+//102
+//False
+//0
