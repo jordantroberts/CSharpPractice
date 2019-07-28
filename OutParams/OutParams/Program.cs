dd01878957d037b3718a38c987c6fde171ef003c
Jordan Roberts <jordanroberts@talk21.com>
2019-07-28 16:55:25 +0100

    Using out on our own methods

--- OutParams/OutParams/Program.cs
+++ OutParams/OutParams/Program.cs
@@ -1,39 +1,26 @@
 ﻿using System;
 
-namespace OutParameters
+namespace UsingOut
 {
-	class Program
-	{
+    class Program
+    {
         static void Main(string[] args)
         {
-            string ageAsString = "102";
-            string nameAsString = "Granny";
-            int ageAsInt;
-            bool outcome;
+            string statement = "GARRRR";
+            string murmur = Whisper(statement, out bool marker);
+            Console.WriteLine(murmur);
+            Console.WriteLine(marker);
+        }
 
-            // For example the Int32.TryParse() method tries to parse its input as an integer.
-            // If it can, it returns true and sets a specific variable to the new value.
-            outcome = Int32.TryParse(ageAsString, out ageAsInt);
-
-            Console.WriteLine(outcome);
-            Console.WriteLine(ageAsInt);
-
-            int nameAsInt;
-            bool outcome2;
-
-            outcome2 = Int32.TryParse(nameAsString, out nameAsInt);
-
-            Console.WriteLine(outcome2);
-            Console.WriteLine(nameAsInt);
-
-
+        static string Whisper(string phrase, out bool wasWhisperCalled)
+        {
+            wasWhisperCalled = true;
+            return phrase.ToLower();
         }
     }
 }
 
-// Output:
 
-//True
-//102
-//False
-//0
+// Output:
+// garrr
+// True
